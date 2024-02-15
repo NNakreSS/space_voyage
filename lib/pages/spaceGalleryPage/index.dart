76640dbd13f2263f79238f276e642dfbdf8d19f2
NNakreSS <serkanatmaca15@gmail.com>
2024-02-15 12:45:35 +0300

    infinity scroll

--- lib/pages/spaceGalleryPage/index.dart
+++ lib/pages/spaceGalleryPage/index.dart
@@ -1,6 +1,7 @@
 // from dart
 import 'dart:async';
 import 'dart:convert';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 // dependency packagets
 import 'package:http/http.dart' as http;
@@ -19,82 +20,94 @@ class SpaceImages extends StatefulWidget {
 }
 
 class _SpaceImages extends State<SpaceImages> {
-  late Future<List<NasaImage>> _futureImages;
+  late List<NasaImage> _images;
+  bool _isLoading = false;
+  late ScrollController _scrollController;
 
   @override
   void initState() {
     super.initState();
-    _futureImages = fetchImages();
+    _images = [];
+    _fetchImages();
+
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _fetchImages();
+      }
+    });
+  }
+
+  Future<void> _fetchImages() async {
+    if (_isLoading) return;
+    setState(() {
+      _isLoading = true;
+    });
+
+    await dotenv.load(fileName: ".env");
+    final String apiKey = dotenv.env['API_KEY'] ?? "DEMO_KEY";
+
+    final Uri uri = Uri.parse(
+        "https://api.nasa.gov/planetary/apod?api_key=$apiKey&count=10");
+
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = jsonDecode(response.body);
+      final List<NasaImage> newImages = jsonData
+          .map((json) => NasaImage.fromJson(json))
+          .where((data) => data.mediaType == "image")
+          .toList();
+      setState(() {
+        _images.addAll(newImages);
+        _isLoading = false;
+      });
+    } else {
+      throw Exception('Failed ${response.statusCode} : connect API ');
+    }
   }
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: FutureBuilder<List<NasaImage>>(
-          future: _futureImages,
-          builder: (context, snapshot) {
-            return snapshot.connectionState == ConnectionState.waiting
-                ? Center(child: SpinKitPulsingGrid(color: Colors.blue[300]!))
-                : snapshot.hasError
-                    ? Center(
-                        child: Text(
-                          "${snapshot.error}",
-                          style:
-                              TextStyle(fontSize: 20, color: Colors.red[300]!),
-                        ),
-                      )
-                    : snapshot.hasData
-                        ? MasonryGridView.builder(
-                            itemCount: snapshot.data!.length,
-                            gridDelegate:
-                                const SliverSimpleGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 2),
-                            itemBuilder: (BuildContext context, int index) {
-                              NasaImage image = snapshot.data![index];
-                              return Padding(
-                                padding: const EdgeInsets.all(3.0),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(5),
-                                  child: GestureDetector(
-                                    child: Image.network(image.url),
-                                    onTap: () => Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => ImageDetails(
-                                          image: image,
-                                        ),
-                                      ),
+        body: _images.isEmpty
+            ? const SpinKitPulsingGrid(color: Colors.white)
+            : Column(
+                children: [
+                  Expanded(
+                    child: (MasonryGridView.builder(
+                        controller: _scrollController,
+                        itemCount: _images.length,
+                        gridDelegate:
+                            const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2),
+                        itemBuilder: (BuildContext context, int index) {
+                          NasaImage image = _images[index];
+                          return Padding(
+                            padding: const EdgeInsets.all(3.0),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(5),
+                              child: GestureDetector(
+                                child: CachedNetworkImage(imageUrl: image.url),
+                                onTap: () => Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => ImageDetails(
+                                      image: image,
                                     ),
                                   ),
                                 ),
-                              );
-                            })
-                        : const Center(
-                            child: Text(
-                              "Image not found",
-                              style: TextStyle(fontSize: 24, color: Colors.red),
+                              ),
                             ),
                           );
-          },
-        ),
+                        })),
+                  ),
+                  if (_isLoading)
+                    Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: SpinKitPulsingGrid(color: Colors.blue[300]!),
+                    )
+                ],
+              ),
       );
-}
-
-Future<List<NasaImage>> fetchImages() async {
-  await dotenv.load(fileName: ".env");
-  final String apiKey = dotenv.env['API_KEY'] ?? "DEMO_KEY";
-
-  final Uri uri =
-      Uri.parse("https://api.nasa.gov/planetary/apod?api_key=$apiKey&count=50");
-
-  final response = await http.get(uri);
-
-  if (response.statusCode == 200) {
-    final List<dynamic> jsonData = jsonDecode(response.body);
-    return jsonData
-        .map((json) => NasaImage.fromJson(json))
-        .where((data) => data.mediaType == "image")
-        .toList();
-  } else {
-    throw Exception('Failed ${response.statusCode} : connect API ');
-  }
 }
