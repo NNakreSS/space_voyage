2fa59c01a330f1babcf05660ced2f660e9f96805
NNakreSS <serkanatmaca15@gmail.com>
2024-02-19 23:56:20 +0300

    add download notification

--- lib/pages/spaceGalleryPage/details.dart
+++ lib/pages/spaceGalleryPage/details.dart
@@ -1,21 +1,33 @@
-import 'dart:io';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_file_downloader/flutter_file_downloader.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:space_voyage/pages/spaceGalleryPage/model.dart';
 
-class ImageDetails extends StatelessWidget {
+class ImageDetails extends StatefulWidget {
   final NasaImage image;
 
   const ImageDetails({Key? key, required this.image}) : super(key: key);
+  @override
+  State<ImageDetails> createState() => _ImageDetailsState();
+}
+
+class _ImageDetailsState extends State<ImageDetails> {
+  late String _downloadStatus;
+
+  @override
+  void initState() {
+    super.initState();
+    _downloadStatus = 'idle';
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
           title: Tooltip(
-              message: image.title,
+              message: widget.image.title,
               child: Text(
-                image.title!,
+                widget.image.title!,
                 style: const TextStyle(color: Colors.white),
               )),
           backgroundColor: Colors.black,
@@ -27,7 +39,7 @@ class ImageDetails extends StatelessWidget {
           children: [
             Stack(children: [
               Image.network(
-                image.hdurl ?? image.url!,
+                widget.image.hdurl ?? widget.image.url!,
                 loadingBuilder: (BuildContext context, Widget child,
                         ImageChunkEvent? loadingProgress) =>
                     loadingProgress == null
@@ -45,8 +57,9 @@ class ImageDetails extends StatelessWidget {
                 right: 10.0,
                 bottom: 70.0,
                 child: GestureDetector(
-                  onTap: () =>
-                      downloadImage(image.hdurl ?? image.url, image.title!),
+                  onTap: () => downloadImage(
+                      widget.image.hdurl ?? widget.image.url,
+                      widget.image.title!),
                   child: const Icon(
                     size: 40,
                     Icons.download,
@@ -63,6 +76,60 @@ class ImageDetails extends StatelessWidget {
                   color: Colors.white,
                 ),
               ),
+              if (_downloadStatus == 'downloading')
+                Positioned(
+                  left: 10,
+                  bottom: 10,
+                  child: Container(
+                    height: 50,
+                    width: 250,
+                    decoration: const BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius:
+                            BorderRadius.all(Radius.elliptical(10, 10))),
+                    child: const Center(
+                      child: Text(
+                        "Downloading...",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ).animate().slide(
+                      begin: const Offset(-1, 0),
+                      duration: const Duration(milliseconds: 500)),
+                ),
+              if (_downloadStatus == 'success' || _downloadStatus == 'error')
+                Positioned(
+                    left: 10,
+                    bottom: 10,
+                    child: Container(
+                      height: 50,
+                      width: 250,
+                      decoration: BoxDecoration(
+                          color: _downloadStatus == 'success'
+                              ? Colors.green
+                              : Colors.red,
+                          borderRadius: const BorderRadius.all(
+                              Radius.elliptical(10, 10))),
+                      child: Center(
+                        child: Text(
+                          _downloadStatus == 'success'
+                              ? "Download Success"
+                              : "Download Failed",
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    )
+                        .animate()
+                        .fadeIn(duration: const Duration(seconds: 1))
+                        .fadeOut(
+                          duration: const Duration(seconds: 4),
+                        )),
             ]),
             Padding(
               padding: const EdgeInsets.all(16.0),
@@ -71,7 +138,7 @@ class ImageDetails extends StatelessWidget {
                   children: [
                     Expanded(
                       child: Text(
-                        image.date!,
+                        widget.image.date!,
                         style: const TextStyle(
                             fontSize: 16.0,
                             fontWeight: FontWeight.normal,
@@ -81,9 +148,9 @@ class ImageDetails extends StatelessWidget {
                     ),
                     Expanded(
                       child: Tooltip(
-                        message: image.copyright ?? "Nasa",
+                        message: widget.image.copyright ?? "Nasa",
                         child: Text(
-                          "©️ ${image.copyright ?? "Nasa"}",
+                          "©️ ${widget.image.copyright ?? "Nasa"}",
                           overflow: TextOverflow.ellipsis,
                           style: const TextStyle(
                             fontSize: 16.0,
@@ -100,7 +167,7 @@ class ImageDetails extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: Text(
-                image.explanation!,
+                widget.image.explanation!,
                 style: const TextStyle(
                   fontSize: 16.0,
                   color: Colors.white,
@@ -111,14 +178,30 @@ class ImageDetails extends StatelessWidget {
           ],
         ),
       );
-}
 
-Future<void> downloadImage(String? url, String name) async {
-  FileDownloader.downloadFile(
-      url: url!,
-      name: name,
-      onDownloadCompleted: (path) {
-        final File file = File(path);
-        print(file);
-      });
+  // file downloader
+  Future<void> downloadImage(String? url, String name) async {
+    setState(() {
+      _downloadStatus = 'downloading';
+    });
+    try {
+      FileDownloader.downloadFile(
+          url: url!,
+          name: name,
+          onDownloadCompleted: (path) {
+            setState(() => _downloadStatus = 'success');
+
+            Future.delayed(
+              const Duration(seconds: 4),
+              () => setState(() => _downloadStatus = 'idle'),
+            );
+          });
+    } catch (e) {
+      setState(() => _downloadStatus = 'error');
+      Future.delayed(
+        const Duration(seconds: 4),
+        () => setState(() => _downloadStatus = 'idle'),
+      );
+    }
+  }
 }
