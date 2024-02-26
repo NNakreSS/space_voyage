005e88696d251a6773cb6d29c1bb556505e371aa
NNakreSS <serkanatmaca15@gmail.com>
2024-02-26 14:18:42 +0300

    extract methods

--- lib/pages/SignPage/sign_in.dart
+++ lib/pages/SignPage/sign_in.dart
@@ -31,83 +31,102 @@ class _SignInPageState extends State<SignInPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                margin: const EdgeInsets.all(30),
-                child: const Text(
-                  "Sign In",
-                  style: TextStyle(fontSize: 40, color: Colors.white),
-                ),
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.9,
-                child: CustomTextFormField(
-                  controller: _usermailController,
-                  labelText: "Mail",
-                  icon: Icons.mail,
-                ),
-              ),
+              header(),
+              mailField(context),
               const SizedBox(height: 20.0),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.9,
-                child: CustomTextFormField(
-                  controller: _passwordController,
-                  labelText: "Password",
-                  icon: Icons.lock,
-                ),
-              ),
+              passwordField(context),
               const SizedBox(height: 20.0),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(5))),
-                  fixedSize:
-                      Size.fromWidth(MediaQuery.of(context).size.width / 2),
-                  backgroundColor: Colors.blue,
-                ),
-                onPressed: () async {
-                  final email = _usermailController.text;
-                  final password = _passwordController.text;
-                  AuthService().signIn(email: email, password: password);
-                },
-                child: const Text(
-                  'Login',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w800,
-                  ),
-                ),
-              ),
+              loginButton(context),
               const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Not registered yet ?',
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SignUpPage()),
-                      );
-                    },
-                    child: Text(
-                      'Create an account',
-                      style: TextStyle(
-                        color: Colors.blue[100]!,
-                        fontSize: 16.0,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              createAccountTextButton(context),
             ],
           ),
         ),
       );
+
+  Container header() {
+    return Container(
+      margin: const EdgeInsets.all(30),
+      child: const Text(
+        "Sign In",
+        style: TextStyle(fontSize: 40, color: Colors.white),
+      ),
+    );
+  }
+
+  Row createAccountTextButton(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          'Not registered yet ?',
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 16.0,
+          ),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SignUpPage()),
+            );
+          },
+          child: Text(
+            'Create an account',
+            style: TextStyle(
+              color: Colors.blue[100]!,
+              fontSize: 16.0,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  ElevatedButton loginButton(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(5))),
+        fixedSize: Size.fromWidth(MediaQuery.of(context).size.width / 2),
+        backgroundColor: Colors.blue,
+      ),
+      onPressed: () async {
+        final email = _usermailController.text;
+        final password = _passwordController.text;
+        AuthService().signIn(email: email, password: password);
+      },
+      child: const Text(
+        'Login',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+    );
+  }
+
+  SizedBox passwordField(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.9,
+      child: CustomTextFormField(
+        controller: _passwordController,
+        labelText: "Password",
+        icon: Icons.lock,
+      ),
+    );
+  }
+
+  SizedBox mailField(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.9,
+      child: CustomTextFormField(
+        controller: _usermailController,
+        labelText: "Mail",
+        icon: Icons.mail,
+      ),
+    );
+  }
 }
